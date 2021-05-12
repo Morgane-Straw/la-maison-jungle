84ae3dac2b7e35e59e3030b1f71ba5d024c3d45a
Morgane-Straw <58706260+Morgane-Straw@users.noreply.github.com>
2021-05-12 10:05:40 +0200

    style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,13 @@
-import '../styles/Banner.css'
+import "../styles/Banner.css";
+import leaf from "../assets/leaf.png";
 function Banner() {
-  return <div className='lmj-banner'>
-  <h1>La maison jungle</h1>
-</div>
+  const title = "La maison jungle";
+  return (
+    <div className="lmj-banner">
+      <img src={leaf} alt="feuille logo" className="lmj-logo" />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
 export default Banner;
